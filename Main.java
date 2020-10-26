0bf023372366f77c892ecde41de7660a34c36de0
repl.it user <replituser@example.com>
2020-10-26 14:38:52 +0000

    Assignment 1

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello Jonas!");
+    for (int i=1; i<100; i++) {
+      if (i%5 == 0)
+      System.out.println("ZAHL ist durch 5 teilbar");
+
+      if (i%10 == 9)
+      System.out.println("ZAHL endet auf 9");
+
+      if ((i+(i-1))%3 == 0)
+      System.out.println("ZAHL und VORGÄNGER addiert ergeben ERGEBNIS und ERGEBNIS ist durch 3 teilbar");
+    }
   }
 }
